3279db844729bc262336b7f024610c640b9b43a9
Victor Vaca <victorhvr.net@gmail.com>
2026-02-23 21:35:09 -0500

    cadena nueva

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, Platzi!");
 
 string stripe_api_key="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";
+
+string testingConection = "Server=localhost,1433;Database=TestDB;User Id=testuser;Password=Test1234;";
